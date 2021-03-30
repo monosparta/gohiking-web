968386a7e679762c649d2e1bb1467180ef90424a
joesong7 <72059440+joesong7@users.noreply.github.com>
2021-03-30 15:34:38 +0800

    修改Navigation  步道搜尋 href

--- src/components/Bottom/Navigation.js
+++ src/components/Bottom/Navigation.js
@@ -41,7 +41,7 @@ function Navigation(props) {
      >
      
       <BottomNavigationAction label="首頁" href="/home" icon={<HomeIcon />} />
-      <BottomNavigationAction label="步道搜尋" href="/searchResult" icon={<SearchIcon />} />
+      <BottomNavigationAction label="步道搜尋" href="/searchPage" icon={<SearchIcon />} />
       <BottomNavigationAction label="附近步道"  href="/nearbypathway"  icon={ <LocationSearchingIcon />} />
       <BottomNavigationAction label="我的收藏" href="/CollectPage"  icon={<FavoriteIcon  />} />
      </BottomNavigation>
