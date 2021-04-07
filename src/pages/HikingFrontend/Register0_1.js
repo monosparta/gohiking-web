7064c54282a2720eb71c04d7dc6245eaa83cdd09
peter0512lee <peter0512lee@gmail.com>
2021-04-07 21:24:44 +0800

    fix 0.0

--- src/pages/HikingFrontend/Register0_1.js
+++ src/pages/HikingFrontend/Register0_1.js
@@ -4,6 +4,9 @@ import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Input from '@material-ui/core/Input'
 import Typography from '@material-ui/core/Typography';
+import FormControl from "@material-ui/core/FormControl";
+import FormHelperText from '@material-ui/core/FormHelperText';
+
 import { makeStyles } from '@material-ui/core/styles';
 import DateFnsUtils from '@date-io/date-fns';
 import { useForm } from 'react-hook-form';
@@ -31,20 +34,20 @@ const useStyles = makeStyles((theme) => ({
     },
     Title: {
         width: '-webkit-fill-available',
-        height: '36',
-        margin: '0 232px 31px 0',
+        height: '36px',
+        margin: '0 0px 31px 0',
         fontFamily: "NotoSansCJKtc",
         fontSize: '24px',
-        fontweight: '500',
+        fontWeight: 'normal',
         fontStretch: 'normal',
         fontStyle: 'normal',
-        lineheight: '1.5',
-        letterspacing: '0.5px',
+        lineHeight: '1.5',
+        letterSpacing: '0.5px',
         color: '#232323'
     },
     Text:{
-        width: '33',
-        height:'24',
+        // width: '33px',
+        // height:'24px',
         margin: '0 0 1px 0',
         fontSize: '16px',
         fontWeight: '500',
@@ -56,26 +59,26 @@ const useStyles = makeStyles((theme) => ({
     },
     InputBackground:{
         width: '-webkit-fill-available',
-        height: '40',
+        // height: '40px',
         margin: '8px 0 31px 0',
         borderColor:'#232323',
     },
     PhoneRegionBackground:{
         width: '123px',
-        height: '40',
+        // height: '40px',
         margin: '8px 0 31px 0',//not correct position
         padding: '8px 0 0',
     },
     PhoneNumberBackground:{
         width: 'auto',
         //fullWidth:true,
-        height: '40',
+        height: '40px',
         margin: '-12px 0 0px 40px',//first value need to fix
         padding: '8px 0 0',
     },
     submit: {
         width: '-webkit-fill-available',
-        height: '48px',
+        // height: '48px',
         margin: '40px 0px 0px 0px',
         backgroundColor: '#00d04c',
         color: '#ffffff',
@@ -85,7 +88,20 @@ const useStyles = makeStyles((theme) => ({
         fontSize: '14px',
         color: '#979797',  
         borderColor:'#979797'
-      },
+    },
+    errorInfo: {
+      width: '100%',
+      height: '0px',
+      margin: '-20px 0px 30px 0px',
+      fontFamily: 'NotoSansCJKtc',
+      fontSize: '14px',
+      fontWeight: 'normal',
+      fontStretch: 'normal',
+      fontStyle: 'normal',
+      lineHeight: '1.5',
+      letterSpacing: '0.5px',
+      color: '#ff3b30',
+    },
   }));
   const usePlaceholderStyles = makeStyles(theme => ({
     placeholder: {
@@ -94,104 +110,117 @@ const useStyles = makeStyles((theme) => ({
   }));
   ;
 
-  export default function SignIn() {
-    const classes = useStyles();
-    const [name, setName] = React.useState('');
-    const [gender, setGender] = React.useState('');
-    const [phoneRegion, setPhoneRegion] = React.useState('');
-    const [phoneNumeber, setPhoneNumeber] = React.useState('');
-    const [selectedDate, setSelectedDate] = React.useState('');
-    const [inputValue, setInputValue] = useState([]);
-    const [county, setCounty] = React.useState('');
-    const { handleSubmit } = useForm()
-    const history = useHistory();
+export default function SignIn() {
+  const classes = useStyles();
+  const [name, setName] = React.useState('');
+  const [gender, setGender] = React.useState('');
+  const [phoneRegion, setPhoneRegion] = React.useState('');
+  const [phoneNumeber, setPhoneNumeber] = React.useState('');
+  const [selectedDate, setSelectedDate] = React.useState('');
+  const [inputValue, setInputValue] = useState([]);
+  const [county, setCounty] = React.useState('');
+  const { register, handleSubmit, errors, watch } = useForm()
+  const history = useHistory();
+  const [genderError, setGenderError] = React.useState(false);
+  const [countryError, setCountryError] = React.useState(false);
+  const [birthError, setBirthError] = React.useState(false);
+  const [countyError, setCountyError] = React.useState(false);
 
-    const handleChange = (event) => {
-      setGender(event.target.value);
-    };
-    const handlePhoneRegion = (event) => {
-      setPhoneRegion(event.target.value);
-    };
-    const handleCountyChange = (event) => {
-      setCounty(event.target.value);
-    };
-    const handleDateChange = (date, value) => {
-      setSelectedDate(date);
-      setInputValue(value);
-    };
-    
-    const Placeholder = ({ children }) => {
-      const classes = usePlaceholderStyles();
-      return <div className={classes.placeholder}>{children}</div>;
-    };
+  const handleChange = (event) => {
+    setGender(event.target.value);
+  };
+  const handlePhoneRegion = (event) => {
+    setPhoneRegion(event.target.value);
+  };
+  const handleCountyChange = (event) => {
+    setCounty(event.target.value);
+  };
+  const handleDateChange = (date, value) => {
+    setSelectedDate(date);
+    setInputValue(value);
+  };
+  
+  const Placeholder = ({ children }) => {
+    const classes = usePlaceholderStyles();
+    return <div className={classes.placeholder}>{children}</div>;
+  };
 
-    let testOuputObj = {
-      name: name,
-      gender: gender,
-      phone_number: phoneNumeber,
-      country_code_id: phoneRegion,
-      birth: inputValue,
-      county_id: county
-    }
-    const headers = {
-      'Authorization': 'Bearer '+localStorage.getItem('token')
-    }
-    // console.log(headers);
-    const axios = require('axios');
-    let responsedJson; // 將回傳的JSON先定義為變數，後面再賦值
-    const onSubmit = async(data) => {
-      data = testOuputObj;
-      console.log(data);
-      console.log(headers);
-      await axios.post('https://staging-server.gohiking.app/api/profile', data, { headers })
-      .then(function (response) {
-        console.log('correct');
-        responsedJson = response.data;
-      })
-      .catch(function (error) {
-        console.log('error');
-        responsedJson = error.response.data;
-      })
-      .finally(function () {
-        console.log(responsedJson);
-        history.push('/signin');
-      }); 
-    }
+  let testOuputObj = {
+    name: name,
+    gender: gender,
+    phone_number: phoneNumeber,
+    country_code_id: phoneRegion,
+    birth: inputValue,
+    county_id: county
+  }
+  const headers = {
+    'Authorization': 'Bearer '+localStorage.getItem('token')
+  }
+  const axios = require('axios');
+  let responsedJson; // 將回傳的JSON先定義為變數，後面再賦值
+  const onSubmit = async(data) => {
+    data = testOuputObj;
+    console.log(data);
+    console.log(headers);
+    await axios.post('https://staging-server.gohiking.app/api/profile', data, { headers })
+    .then(function (response) {
+      console.log('correct');
+      responsedJson = response.data;
+      history.push('/signin');
+    })
+    .catch(function (error) {
+      console.log('error');
+      responsedJson = error.response.data;
+    })
+    .finally(function () {
+      console.log(responsedJson);
+    }); 
+  }
+  
 
-    return (
-        <div className={classes.container}>
-          <Typography className={classes.Title } >
-            建立個人資料
-          </Typography>
-          <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
-          <Typography className={classes.Text}>
-            姓名 
-          </Typography>
-          <Input 
+  return (
+    <div className={classes.container}>
+      <Typography className={classes.Title}>
+        建立個人資料
+      </Typography>
+      <form className={classes.form}>
+        <Typography className={classes.Text}>
+          姓名 
+        </Typography>
+        <Input 
+          name="name"
           className={classes.InputBackground} 
           placeholder="請輸入您的名稱" 
           fullWidth 
+          inputRef={register({ required: true })}
           onChange={event => setName(event.target.value)}//Get value in Email
-          />
-          <Typography className={classes.Text} >
-            性別
-          </Typography>  
-          <Select
+        />
+        <Typography className={classes.errorInfo}>{errors.name && "請輸入姓名"}</Typography>
+        <Typography className={classes.Text} >
+          性別
+        </Typography>
+        <FormControl error={genderError}>
+        <Select
           className={classes.InputBackground}
           value={gender}
           displayEmpty
           renderValue={
             gender !== "" ? undefined : () => <Placeholder>請輸入您的性別</Placeholder>
           }
+          naitve
+          inputProps={register({ required: true })}
           onChange={handleChange}
-          >   
-            <MenuItem value={1}>男</MenuItem>
-            <MenuItem value={0}>女</MenuItem>
-          </Select>
-          <Typography className={classes.Text} >
-            手機
-          </Typography>
-          <div>
+        >   
+          <MenuItem value={1}>男</MenuItem>
+          <MenuItem value={0}>女</MenuItem>
+        </Select>
+        {genderError && <Typography className={classes.errorInfo}>This is required!</Typography>}
+        </FormControl>
+        <Typography className={classes.Text} >
+          手機
+        </Typography>
+        <div>
+        <FormControl error={countryError}>
           <Select
           className={classes.PhoneRegionBackground}
           value={phoneRegion}
@@ -204,37 +233,47 @@ const useStyles = makeStyles((theme) => ({
             <MenuItem value={4}>台灣+886</MenuItem>
             <MenuItem value={424}>香港+852</MenuItem>
           </Select>
+          {countryError && <Typography className={classes.errorInfo}>This is required!</Typography>}
+          </FormControl>
           <Input 
-          className={classes.PhoneNumberBackground} 
-          placeholder="請輸您的手機號碼"  
-          onChange={event => setPhoneNumeber(event.target.value)}//Get value in Email
-          fullWidth/>
-          </div>
-          <Typography className={classes.Text} >
-            生日
-          </Typography>
-          <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <KeyboardDatePicker
-            placeholder="請選擇"
-            className={classes.InputBackground}
-            disableToolbar
-            variant="inline"
-            format="yyyy/MM/dd"
-            margin="normal"
-            invalidDateMessage=''
-            id="date-picker-inline"
-            value={selectedDate}
-            inputValue={inputValue}
-            onChange={handleDateChange}
-            KeyboardButtonProps={{
-              'aria-label': 'change date',
-            }}
+            name="phoneNumber"
+            className={classes.PhoneNumberBackground} 
+            placeholder="請輸您的手機號碼"  
+            onChange={event => setPhoneNumeber(event.target.value)}//Get value in Email
+            fullWidth
+            inputRef={register({ required: true })}
           />
-          </MuiPickersUtilsProvider>
-          <Typography className={classes.Text} >
-            居住地
-          </Typography>
-          <Select
+          <Typography className={classes.errorInfo}>{errors.phoneNumber && "請輸入手機號碼"}</Typography>
+        </div>
+        <Typography className={classes.Text} >
+          生日
+        </Typography>
+        <FormControl error={birthError}>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDatePicker
+          placeholder="請選擇"
+          className={classes.InputBackground}
+          disableToolbar
+          variant="inline"
+          format="yyyy/MM/dd"
+          margin="normal"
+          invalidDateMessage=''
+          id="date-picker-inline"
+          value={selectedDate}
+          inputValue={inputValue}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+        </MuiPickersUtilsProvider>
+        {birthError && <Typography className={classes.errorInfo}>This is required!</Typography>}
+        </FormControl>
+        <Typography className={classes.Text} >
+          居住地
+        </Typography>
+        <FormControl error={countyError}>
+        <Select
           className={classes.InputBackground}
           value={county}
           displayEmpty
@@ -242,19 +281,22 @@ const useStyles = makeStyles((theme) => ({
             county !== "" ? undefined : () => <Placeholder>請選擇</Placeholder>
           }
           onChange={handleCountyChange}
-          >   
-            <MenuItem value={"4"}>台北市</MenuItem>
-            <MenuItem value={"14"}>台中市</MenuItem>
-          </Select>
-          <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              className={classes.submit}
-            >
-            同意並註冊
-          </Button>
-        </form>
-      </div>
-    );
-  }
+        >   
+          <MenuItem value={"4"}>台北市</MenuItem>
+          <MenuItem value={"14"}>台中市</MenuItem>
+        </Select>
+        {countyError && <Typography className={classes.errorInfo}>This is required!</Typography>}
+        </FormControl>
+        <Button
+          type="button"
+          fullWidth
+          variant="contained"
+          className={classes.submit}
+          onClick={handleSubmit(onSubmit)}
+        >
+          同意並註冊
+        </Button>
+     </form>
+    </div>
+  );
+}
