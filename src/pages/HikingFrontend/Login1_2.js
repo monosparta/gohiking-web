d9200dfe53dd349b3befe4c2f52a89a61912ee3e
unknown <s1061421@mail.yzu.edu.tw>
2021-04-09 21:14:29 +0800

    0.1%1.2 防呆

--- src/pages/HikingFrontend/Login1_2.js
+++ src/pages/HikingFrontend/Login1_2.js
@@ -1,4 +1,4 @@
-import React , {useState}from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import { useHistory } from "react-router-dom";
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
@@ -25,23 +25,23 @@ const useStyles = makeStyles((theme) => ({
     height: 768,
     padding: '40px 16px 213px',
     backgroundColor: '#ffffff'
-    
+
     //backgroundColor:'#66CBBA'
   },
-  MaterialIconsBlackArrowback :{
+  MaterialIconsBlackArrowback: {
     width: '24px',
     height: '24px',
     color: '#00d04c',
     margin: '0 297px 61px 0'
   },
-  InputBackground:{
+  InputBackground: {
     width: '-webkit-fill-available',
     height: '40px',
     margin: '1px 0 0',
     padding: '9px 0 0',
-    borderColor:'#232323',
+    borderColor: '#232323',
   },
-  Title:{
+  Title: {
     width: '98px',
     height: '36px',
     margin: '0 281px 31px 0',
@@ -52,9 +52,9 @@ const useStyles = makeStyles((theme) => ({
     letterspacing: '0.5px',
     color: '#232323'
   },
-  Text:{
+  Text: {
     width: '66px',
-    height:'24px',
+    height: '24px',
     margin: '0 313px 1px 0',
     fontSize: '16px',
     fontWeight: '500',
@@ -64,7 +64,7 @@ const useStyles = makeStyles((theme) => ({
     letterSpacing: '0.5px',
     color: '#232323',
   },
-  ErrorInfo:{
+  ErrorInfo: {
     width: '100%',
     height: '21px',
     margin: '16px 263px 56px 0',
@@ -77,7 +77,7 @@ const useStyles = makeStyles((theme) => ({
     letterSpacing: '0.5px',
     color: '#ff3b30',
   },
-  ForgotInfo:{
+  ForgotInfo: {
     width: '58px',
     height: '21px',
     fontFamily: "NotoSansCJKtc",
@@ -93,14 +93,16 @@ const useStyles = makeStyles((theme) => ({
   ModifyTextFieldColor: {
     // Theme Color, or use css color in quote
     fontSize: '14px',
-    color: '#979797',  
-    borderColor:'#979797'
+    color: '#979797',
+    borderColor: '#979797'
   },
 }));
 
 export default function SignIn() {
   const classes = useStyles();
   const { register, handleSubmit, errors } = useForm()
+  const [errormassage, setErrormassage] = React.useState(false);
+  const [captchavalidate, setCaptchavalidate] = React.useState(false);
   const axios = require('axios');
   let responsedJSON;
   let back = useHistory();
@@ -110,68 +112,92 @@ export default function SignIn() {
   }
   function onChange(value) {
     console.log("Captcha value:", value);
+    setCaptchavalidate(true);
+    setErrormassage(false);
   }
   function backhandleClick() {
     back.push("/login1_1");
   }
-
+  console.log(errormassage);
   // API POST
   const onSubmit = async (data) => {
     console.log(data);
-    await axios.post('https://staging-server.gohiking.app/api/password/forget', data)
-    .then(function (response) {
-      console.log('correct');
-      const { token } = response.data;
-      responsedJSON = response.data
-      localStorage.setItem('token', token)
-      localStorage.setItem('email',email)
+    if (captchavalidate) {
+      await axios.post('https://staging-server.gohiking.app/api/password/forget', data)
+        .then(function (response) {
+          console.log('correct');
+          const { token } = response.data;
+          responsedJSON = response.data
+          localStorage.setItem('token', token)
+          localStorage.setItem('email', email)
 
-      ResetPassword()
-    })
-    .catch(function (error) {
-      console.log('error');
-      responsedJSON = error.response.data;
-    })
-    .finally(function () {
-      console.log(responsedJSON);
-    });  
+          ResetPassword()
+        })
+        .catch(function (error) {
+          console.log('error');
+          responsedJSON = error.response.data;
+        })
+        .finally(function () {
+          console.log(responsedJSON);
+        });
+    }
+    else {
+      setErrormassage(true);
+    }
   }
-
-  const[email, setEmail] = useState(-1);
-  const handleChange = (event) =>{
+  function handleErrorMessage() {
+    if (captchavalidate) {
+      setErrormassage(false);
+    }
+    else {
+      setErrormassage(true);
+    }
+  }
+  const [email, setEmail] = useState(-1);
+  const handleChange = (event) => {
     setEmail(event.target.value);
   }
 
   return (
-      <div className={classes.container}>
-        <ArrowBackIcon className={classes.MaterialIconsBlackArrowback} onClick={backhandleClick} ></ArrowBackIcon>
-        <Typography className={classes.Title} textalign="left">
-          忘記密碼
+    <div className={classes.container}>
+      <ArrowBackIcon className={classes.MaterialIconsBlackArrowback} onClick={backhandleClick} ></ArrowBackIcon>
+      <Typography className={classes.Title} textalign="left">
+        忘記密碼
         </Typography>
-        <form className={classes.form} onSubmit={handleSubmit(onSubmit)} noValidate>
-        <Typography className={classes.Text}  textalign="left">
-          電子信箱 
-        </Typography>   
-          <Input
-            onChange = {event => handleChange(event)}
-            inputRef={register({ required: true, minLength: 8 })}
-            className={classes.InputBackground}
-            id="email"
-            label="請輸入你的電子信箱"
-            name="email"
-          />
+      <form className={classes.form} onSubmit={handleSubmit(onSubmit)} noValidate>
+        <Typography className={classes.Text} textalign="left">
+          電子信箱
+        </Typography>
+        <Input
+          onChange={event => handleChange(event)}
+          inputRef={register({ required: true, minLength: 8 })}
+          className={classes.InputBackground}
+          id="email"
+          label="請輸入你的電子信箱"
+          name="email"
+        />
         <Typography className={classes.ErrorInfo} >
           {errors.email && "請輸入電子信箱"}
-        </Typography>  
+        </Typography>
         <ReCAPTCHA
-            sitekey="6LdFiEYaAAAAAHcdu_AyzIktEbqdTz7pXmNBC__W"
-            onChange={onChange}
-         />
+          sitekey="6LdFiEYaAAAAAHcdu_AyzIktEbqdTz7pXmNBC__W"
+          onChange={onChange}
+        />
+        {errormassage ?
+          <>
+            <Typography className={classes.ErrorInfo} >
+              {"請輸入電子信箱"}
+            </Typography>
+          </>
+          :
+          <div></div>
+        }
         <Button
-            type="submit"
-            variant="contained"
-            className={classes.submit}
-          >
+          onClick={handleErrorMessage}
+          type="submit"
+          variant="contained"
+          className={classes.submit}
+        >
           繼續
         </Button>
       </form>
