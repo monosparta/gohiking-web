c17cf372adb74241f884db98054060e3b8fc242c
FanFan <eric93504@gmail.com>
2021-04-15 11:31:45 +0800

    步道篩選加上uuid

--- src/pages/SearchResult/SearchResult.js
+++ src/pages/SearchResult/SearchResult.js
@@ -44,19 +44,23 @@ function SearchResult(props) {
   }, [kw]);
   //搜尋function
   const searchApi = async kw => {
-    let uid="";
+    let uid = "";
     if (localStorage.getItem("userId")) {
       uid = localStorage.getItem("userId");
     }
     await demoapi
-      .get("/api/trail?filters=title:" + kw + "&uuid="+uid)
+      .get("/api/trail?filters=title:" + kw + "&uuid=" + uid)
       .then(res => {
         setSearchResult(res.data);
       });
   };
   //搜尋function2
   const searchApiSlideBar = async url => {
-    await demoapi.get(url).then(res => {
+    let uid = "";
+    if (localStorage.getItem("userId")) {
+      uid = localStorage.getItem("userId");
+    }
+    await demoapi.get(url + "&uuid=" + uid).then(res => {
       setSearchResult(res.data);
     });
   };
